fcebd95579449cbdf49371776d0f476b4ec60c90
SergeyNefyodov <nefyodov.sergei@gmail.com>
2024-06-18 16:31:08 +0200

    Remove R20 configuration

---
+++ ExtensibleStorageExample/Views/MapFieldEditor.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using ExtensibleStorageExample.Models;
+
+namespace ExtensibleStorageExample.Views
+{
+    public partial class MapFieldEditor : Window
+    {
+        public MapFieldEditor(MapFieldDescriptor descriptor)
+        {
+            DataContext = descriptor;
+            InitializeComponent();
+        }
+    }
+}
